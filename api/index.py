a9778e79f55df819ad470c343b5f0ff9575dc8f3
tburnham <tburnham@villagemd.com>
2020-04-22 19:34:21 -0500

    Import extensions from respective modules and then use init_app() functions within create_app factory method

--- api/index.py
+++ api/index.py
@@ -1,24 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
 
 from api import version_info
 from api import routes, config
+from api.controllers.auth import jwt
+from api.models import db
 
 
 def create_app(app_settings):
     app_name = app_settings.APP_NAME or __name__
     flask_app = Flask(app_name)
     flask_app.config.from_object(app_settings)
+    jwt.init_app(app=flask_app)
+    db.init_app(app=flask_app)
     routes.register(flask_app)
     return flask_app
 
 
 app = create_app(config.FlaskConfig)
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-jwt = JWTManager(app)
 
 
 @app.route('/api')
