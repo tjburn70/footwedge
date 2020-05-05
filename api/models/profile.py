9e5072fe4e6b211f91c276f8f48cd9505184a650
tburnham <tburnham@villagemd.com>
2020-05-04 19:44:03 -0500

    foreign key to the user model and use singular name profile

--- api/models/profile.py
+++ api/models/profile.py
@@ -2,9 +2,9 @@ from . import db
 
 
 class Profile(db.Model):
-    __tablename__ = "profiles"
+    __tablename__ = "profile"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('public.user.user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('public.user.id'), nullable=False)
     home_course = db.Column(db.String)
     dexterity = db.Column(db.String)
 
@@ -37,7 +37,7 @@ class Profile(db.Model):
     def get_by_id(user_id):
         """
         Query a User's Profile by their user id
-        :param: email
+        :param: user_id
         :return: User or None
         """
         return Profile.query.filter_by(user_id=user_id).first()
