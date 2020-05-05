1b21816f7b7336e96657c3f9504b79f34f081958
tburnham <tburnham@villagemd.com>
2020-05-04 19:52:56 -0500

    add auto-increment flag to primary key for user

--- api/models/user.py
+++ api/models/user.py
@@ -11,7 +11,7 @@ from . import db
 
 class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     first_name = db.Column(db.String, nullable=False)
