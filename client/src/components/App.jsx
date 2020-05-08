9ab23b84d5210cdb909d310e7c94f4080fb8cbc7
tburnham <tburnham@villagemd.com>
2020-05-08 14:45:05 -0500

    Register routes in app component and retrieve accessToken if a refreshToken is found in localstorage, also connect the App to the redux store

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,47 +1,72 @@
 import React from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { Route, Switch, Link } from 'react-router-dom';
+import { getAccessToken, loginUser, registerUser } from '../actions/api';
+import PrivateRoute from './PrivateRoute';
+import { NavBar } from './NavBar';
+import { Login } from './Login';
+import { RegisterUser } from './RegisterUser';
+import PlayerProfile from './PlayerProfile';
 
-import { ALL_SQUARE_API_ROOT } from '../constants';
-
-
-const initialState = {
-    isLoggedIn: false,
-}
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
 
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('refresh_token');
+    if (token) {
+      this.props.dispatch(getAccessToken());
     }
+  }
 
-    checkApiHealth = (url) => {
-        axios.get(url)
-        .then(res => {
-           const results = res.data.results;
-           console.log(results);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  render() {
+    const { dispatch, auth, currentUser, errorMessage } = this.props;
 
-    componentDidMount() {
-        let healthPath = "/health"
-        let url = new URL(healthPath, ALL_SQUARE_API_ROOT);
+    return (
+      <div>
+        <div>
+          <NavBar isAuthenticated={auth.isAuthenticated} dispatch={dispatch}/>
+          <Switch>
+            <Route path="/login">
+              <Login
+                loginUser={(data) => dispatch(loginUser(data))}
+                isAuthenticated={auth.isAuthenticated}
+                errorMessage={errorMessage}
+              />
+            </Route>
+            <Route path='/register'>
+              <RegisterUser
+                registerUser={(data) => dispatch(registerUser(data))}
+                errorMessage={errorMessage}
+              />
+            </Route>
+            <PrivateRoute
+              path='/player-profile'
+              component={PlayerProfile}
+              isAuthenticated={auth.isAuthenticated}
+              currentUser={currentUser}>
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 
-        this.checkApiHealth(url);
-    }
-
-    render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        console.log(isLoggedIn);
-        return (
-            <div>
-                "Welcome to AllSquare your go-to Golf app for score-keeping, goal-setting, and course exploration"
-            </div>
-        )
-    }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    auth: {
+      isAuthenticated: state.auth.isAuthenticated,
+      isFetching: state.auth.isFetching,
+      accessToken: state.auth.accessToken,
+    },
+    errorMessage: state.errorMessage,
+    currentUser: state.currentUser,
+  }
+};
+
+export default connect(mapStateToProps)(App);
