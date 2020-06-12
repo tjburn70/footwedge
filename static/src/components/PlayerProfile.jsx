4ca8b345ed072c64509bf4c7f9842a8d85c4a1cd
tburnham <tburnham@villagemd.com>
2020-06-12 18:32:14 -0400

    Overhaul PlayerProfile

--- static/src/components/PlayerProfile.jsx
+++ static/src/components/PlayerProfile.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, Route } from 'react-router';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { EnterRound } from './EnterRound';
+import ScorecardHeader from './ScorecardHeader';
+import Handicap  from './HandicapCard';
+import PastGolfRoundsContainer from './PastGolfRoundsContainer';
+import { enterGolfRound, getHandicap, getGolfRounds } from '../actions/api';
 
-class PlayerProfile extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export const PlayerProfile = (props) => {
+  const {
+    userId,
+    errorMessage,
+    handicap,
+    rounds,
+    stats,
+  } = props.data;
+  const { path } = props.match;
 
-  render() {
-    const { currentUser } = this.props;
+  useEffect(() => {
+    if (userId) {
+      props.dispatch(getHandicap(userId));
+    }
+  }, [userId, handicap.index]);
 
-    console.log(currentUser);
+  useEffect(() => {
+    if (userId) {
+      props.dispatch(getGolfRounds(userId));
+    }
+  }, [userId, Object.keys(rounds).length]);
 
-    return (
-      <div>
-        Your player profile!
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Container>
+        <Grid>
+          <Handicap
+            handicapIndex={handicap.index}
+            calculatedOn={handicap.lastCalculated}
+            enterRoundPath={`${path}/enter-round`}
+          />
+        </Grid>
+        <Grid>
+          <PastGolfRoundsContainer
+            rounds={rounds}
+            stats={stats}
+            dispatch={props.dispatch}
+          />
+        </Grid>
+      </Container>
+
+      <Route path={`${path}/enter-round`}>
+        <EnterRound
+          enterGolfRound={
+            (userId, data) => props.dispatch(enterGolfRound(userId, data))
+          }
+          userId={userId}
+          errorMessage={errorMessage}
+        />
+      </Route>
+    </div>
+  );
 }
-
-export default PlayerProfile;
