2ea8850df79517c6fa89f9dbdc4356b631856aa9
tburnham <tburnham@villagemd.com>
2020-06-16 08:03:21 -0500

    Remove unused useState import

--- static/src/components/PlayerProfile.jsx
+++ static/src/components/PlayerProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link, Route } from 'react-router';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
