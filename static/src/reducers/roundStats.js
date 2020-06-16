ac7c8d539413ac82b1fa92a3b168b94d4549f8fa
tburnham <tburnham@villagemd.com>
2020-06-16 09:25:42 -0500

    remove unused stat import

--- static/src/reducers/roundStats.js
+++ static/src/reducers/roundStats.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { normalize } from 'normalizr';
-import { round, stat } from '../actions/schema';
+import { round } from '../actions/schema';
 
 import {
   ADD_ROUND_STAT_SUCCESS,
