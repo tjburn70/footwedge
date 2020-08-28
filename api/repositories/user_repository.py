26f764e4b4810edfd37ffb6f4b7036da7cd0546d
Tristan Burnham <tjburn70@gmail.com>
2020-08-28 16:51:05 -0500

    Add a reset password method to the UserRepository

--- api/repositories/user_repository.py
+++ api/repositories/user_repository.py
@@ -1,5 +1,8 @@
 from api.repositories.base_repository import BaseRepository
 from api.models import User
+from api.schemas import UserSchema
+
+user_schema = UserSchema()
 
 
 class UserRepository(BaseRepository):
@@ -11,6 +14,17 @@ class UserRepository(BaseRepository):
         :return: User or None
         """
         return self.db_session.query(self.model).filter_by(email=email).first()
+
+    def reset_password(self, user_obj: User, new_password: str) -> User:
+        """
+        Update/reset the user password.
+        :param: user_obj
+        :param: new_password
+        :return: User
+        """
+        user_obj.password_hash = User.hash_password(password=new_password)
+        user_data = user_schema.dump(user_obj)
+        return self.update(data=user_data)
 
 
 user_repo = UserRepository(model=User)
