e369e92da6337679a672ae2de5d987df5930cbe1
Tristan Burnham <tjburn70@gmail.com>
2020-08-29 18:42:08 -0500

    Remove /handicaps from user controller, since we can get the user_id from the jwt token

--- api/controllers/user.py
+++ api/controllers/user.py
@@ -155,51 +155,6 @@ def reset_password():
     pass
 
 
-@blueprint.route('<int:user_id>/handicaps', methods=['GET', 'POST'])
-@requires_json_content
-@jwt_required
-def handicap(user_id):
-    if request.method == 'GET':
-        start_date = request.args.get('start')
-        if start_date:
-            handicaps = handicap_repo.get_by_date(user_id=user_id, start_date=start_date)
-            results = handicap_schema.dump(handicaps, many=True)
-            response_body = {
-                'status': 'success',
-                'result': results.data,
-            }
-            return make_response(jsonify(response_body), HTTPStatus.OK.value)
-        else:
-            current_handicap = handicap_repo.get_active(user_id=user_id)
-            result = handicap_schema.dump(current_handicap)
-            response_body = {
-                'status': 'success',
-                'result': result.data,
-            }
-            return make_response(jsonify(response_body), HTTPStatus.OK.value)
-
-    current_handicap = handicap_repo.get_active(user_id=user_id)
-    handicap_repo.close(current_handicap)
-    request_body = request.get_json()
-    request_body['user_id'] = user_id
-    try:
-        handicap_data = handicap_schema.load(request_body).data
-    except ValidationError as e:
-        response_body = {
-            'status': 'fail',
-            'message': e.messages
-        }
-        return make_response(jsonify(response_body), HTTPStatus.UNPROCESSABLE_ENTITY.value)
-
-    handicap_repo.create(data=handicap_data)
-    response_body = {
-        'status': 'success',
-        'message': f"Handicap was successfully added for user_id: '{user_id}'",
-        'uri': f'/user/{user_id}/handicaps',
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
-
-
 @blueprint.route('<int:user_id>/golf-rounds', methods=['GET', 'POST'])
 @requires_json_content
 @jwt_required
