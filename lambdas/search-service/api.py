1716cdc12e5acbf01c13140af3b7b44af5f6ce02
Tristan Burnham <tjburn70@gmail.com>
2020-09-24 00:41:23 -0500

    Add more routes to search api

--- lambdas/search-service/api.py
+++ lambdas/search-service/api.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, Request, HTTPException
+from typing import Optional
+
 import uvicorn
+from fastapi import FastAPI, Body
+from elasticsearch import Elasticsearch
 
+from service import SearchService
 
-path_prefix = '/search'
-
-app = FastAPI(openapi_prefix=path_prefix)
 
 SEARCH_ENGINE_URL = "http://localhost:9200"
+app = FastAPI()
+es_client = Elasticsearch(hosts=[SEARCH_ENGINE_URL])
 
 
 @app.get('/health')
@@ -14,19 +17,73 @@ async def root():
     return "Welcome to Search Service"
 
 
-@app.post('/index')
-async def create_index():
-    pass
+@app.get('/')
+async def get_all_indexes():
+    return SearchService(es_client=es_client).get_indices()
 
 
-@app.put('/index')
-async def add_to_index():
-    pass
+@app.post('/{index}')
+async def create_index(index: str, body: dict = Body(...)):
+    return SearchService(es_client=es_client).create_index(index=index, body=body)
 
 
-@app.get('/index')
-async def search_index():
-    pass
+@app.delete('/{index}')
+async def delete_index(index: str):
+    return SearchService(es_client=es_client).delete_index(index=index)
+
+
+# @app.get('/{index}')
+# async def get_index(index: str):
+#     return SearchService(es_client=es_client).get_index(index=index)
+
+
+@app.get('/{index}')
+async def search_index(index: str, q: str, field: Optional[str] = None):
+    search_service = SearchService(es_client=es_client)
+    if field:
+        results = search_service.full_text_search(
+            index=index,
+            text=q,
+            field=field,
+        )
+    else:
+        results = search_service.full_text_search_multi(
+            index=index,
+            text=q,
+        )
+
+    return results
+
+
+@app.get('/{index}/_doc')
+async def get_all_documents(index: str):
+    return SearchService(es_client=es_client).get_all_documents(index=index)
+
+
+@app.get('/{index}/_doc/{doc_id}')
+async def get_document(index: str, doc_id: str):
+    return SearchService(es_client=es_client).get_document(index=index, document_id=doc_id)
+
+
+@app.put('/{index}/_doc')
+async def add_document(index: str, payload: dict = Body(..., embed=True), _id: int = Body(..., embed=True)):
+    search_service = SearchService(es_client=es_client)
+    return search_service.add_document(
+        index=index,
+        payload=payload,
+        document_id=_id,
+    )
+
+
+@app.put('/{index}/_doc/{doc_id}/_update/{nested_property}')
+async def update_doc_nested_property(index: str, doc_id: str, nested_property: str, payload: dict = Body(...)):
+    search_service = SearchService(es_client=es_client)
+    return search_service.add_to_nested_property(
+        index=index,
+        document_id=doc_id,
+        nested_property=nested_property,
+        nested_property_body=payload,
+    )
 
 
 if __name__ == "__main__":
