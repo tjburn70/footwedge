0f0b2c96b76c5dd753494f1aeba3759530d25256
Tristan Burnham <tjburn70@gmail.com>
2020-09-24 01:43:03 -0500

    handle http events to lambda with adapter

--- lambdas/search-service/handler.py
+++ lambdas/search-service/handler.py
@@ -2,27 +2,17 @@ import json
 
 from mangum import Mangum
 
-adapter = Mangum(app, enable_lifespan=False, api_gateway_base_path=path_prefix, log_level='info')
+from api import app
+
+PATH_PREFIX = "/search"
+
+adapter = Mangum(app, enable_lifespan=False, api_gateway_base_path=PATH_PREFIX, log_level='info')
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    print(json.dumps(event))
+    response = adapter.handler(event=event, context=context)
+    print("Response: ", response)
+    print("Scope: ", response.scope)
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
