30e5f0f61b127b37960a1d3b9caac1585a1be5ec
Tristan Burnham <tjburn70@gmail.com>
2020-09-25 16:58:54 -0500

    service class to handle business logic and interface with db

---
+++ api/services/golf_club_service.py
@@ -0,0 +1,60 @@
+from http import HTTPStatus
+
+from flask import (
+    Response,
+    make_response,
+    jsonify,
+)
+from marshmallow import ValidationError
+
+from api.repositories.golf_club_repository import GolfClubRepository
+from api.schemas import GolfClubSchema
+from api.services.search_service import SearchService
+
+
+class GolfClubService:
+
+    def __init__(self, repo: GolfClubRepository, schema: GolfClubSchema):
+        self._golf_club_repo = repo
+        self._golf_club_schema = schema
+
+    def get(self, _id: int) -> Response:
+        golf_club = self._golf_club_repo.get(_id)
+        result = self._golf_club_schema.dump(golf_club)
+        response_body = {
+            'status': 'success',
+            'result': result.data
+        }
+        return make_response(jsonify(response_body), HTTPStatus.OK.value)
+
+    def get_all(self) -> Response:
+        clubs = self._golf_club_repo.get_all()
+        results = self._golf_club_schema.dump(clubs, many=True)
+        response_body = {
+            'status': 'success',
+            'result': results.data
+        }
+        return make_response(jsonify(response_body), HTTPStatus.OK.value)
+
+    def add(self, payload: dict) -> Response:
+        try:
+            golf_club_data = self._golf_club_schema.load(payload).data
+        except ValidationError as e:
+            response_body = {
+                'status': 'fail',
+                'message': e.messages
+            }
+            return make_response(jsonify(response_body), HTTPStatus.UNPROCESSABLE_ENTITY.value)
+
+        golf_club = self._golf_club_repo.create(data=golf_club_data)
+        # TODO: Is this the best way, maybe make an async future
+        SearchService.add_golf_club(
+            golf_club_id=golf_club.id,
+            payload=self._golf_club_schema.dump(golf_club).data
+        )
+        response_body = {
+            'status': 'success',
+            'message': f"Golf Club: '{golf_club.name}' was successfully added",
+            'uri': f'api/golf-clubs/{golf_club.id}',
+        }
+        return make_response(jsonify(response_body), HTTPStatus.OK.value)
