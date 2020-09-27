02e17cd49cd2a7c52d90cf346696b0713c06fa98
Tristan Burnham <tjburn70@gmail.com>
2020-09-27 15:47:15 -0500

    Remove GolfRounds from PlayerProfile

--- static/src/components/PlayerProfile.jsx
+++ static/src/components/PlayerProfile.jsx
@@ -2,35 +2,21 @@ import React, { useEffect } from 'react';
 import { Route } from 'react-router';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { EnterRound } from './EnterRound';
 import Handicap  from './HandicapCard';
-import PastGolfRoundContainer from './PastGolfRoundContainer';
-import { enterGolfRound, getHandicap, getGolfRounds } from '../actions/api';
+import { getHandicap } from '../actions/api';
 
 export const PlayerProfile = (props) => {
   const {
-    userId,
     accessToken,
-    errorMessage,
     handicap,
-    rounds,
-    stats,
   } = props.data;
-  const { path } = props.match;
   const dispatch = props.dispatch;
-  const numRounds = rounds.allIds.length;
 
   useEffect(() => {
     if (accessToken) {
       dispatch(getHandicap());
     }
   }, [accessToken, handicap.index, dispatch]);
-
-  useEffect(() => {
-    if (userId) {
-      dispatch(getGolfRounds(userId));
-    }
-  }, [userId, numRounds, dispatch]);
 
   return (
     <div>
@@ -42,24 +28,7 @@ export const PlayerProfile = (props) => {
             enterRoundPath={`${path}/enter-round`}
           />
         </Grid>
-        <Grid>
-          <PastGolfRoundContainer
-            rounds={rounds}
-            stats={stats}
-            dispatch={dispatch}
-          />
-        </Grid>
       </Container>
-
-      <Route path={`${path}/enter-round`}>
-        <EnterRound
-          enterGolfRound={
-            (userId, data) => dispatch(enterGolfRound(userId, data))
-          }
-          userId={userId}
-          errorMessage={errorMessage}
-        />
-      </Route>
     </div>
   );
 }
