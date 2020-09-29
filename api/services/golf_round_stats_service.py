cccb543bf400f7272c0e63d7be6050053d7bf7f9
Tristan Burnham <tjburn70@gmail.com>
2020-09-29 12:28:58 -0500

    Add a method to GolfRoundStatsService to calculate some basic stats regarding a previous golf_round

--- api/services/golf_round_stats_service.py
+++ api/services/golf_round_stats_service.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import List
 
 from flask import (
     Response,
@@ -7,8 +8,10 @@ from flask import (
 )
 from marshmallow import ValidationError
 
+from api.services.hole_service import HoleService
 from api.repositories.golf_round_stats_repository import GolfRoundStatsRepository
 from api.schemas import GolfRoundStatsSchema
+from api.models import GolfRound, GolfRoundStats
 
 
 class GolfRoundStatsService:
@@ -55,5 +58,53 @@ class GolfRoundStatsService:
             'message': message,
             'uri': f'/golf-rounds/{golf_round_id}/golf-round-stats',
             'result': results.data,
+        }
+        return make_response(jsonify(response_body), HTTPStatus.OK.value)
+
+    @staticmethod
+    def calculate_stats_summary(stats: List[GolfRoundStats], hole_ids_to_par: dict) -> dict:
+        total_putts = 0
+        total_fairways = 0
+        total_greens_in_regulation = 0
+        up_and_downs = 0
+        sand_saves = 0
+        for stat in stats:
+            total_putts += stat.putts
+            if stat.fairway_hit:
+                total_fairways += 1
+            if stat.green_in_regulation:
+                total_greens_in_regulation += 1
+            par = hole_ids_to_par.get(stat.hole_id)
+            if stat.gross_score == par:
+                if stat.putts == 1 and stat.chips == 1:
+                    up_and_downs += 1
+                if stat.putts == 1 and stat.greenside_sand_shots == 1:
+                    sand_saves += 1
+        stats_summary = {
+            'putts': total_putts,
+            'fairways': total_fairways,
+            'greens_in_regulation': total_greens_in_regulation,
+            'up_and_downs': up_and_downs,
+            'sand_saves': sand_saves,
+        }
+        return stats_summary
+
+    def stats_summary_by_round_id(self, golf_rounds: List[GolfRound], hole_service: HoleService) -> Response:
+        data = {}
+        for golf_round in golf_rounds:
+            hole_ids = [stat.hole_id for stat in golf_round.stats]
+            hole_ids_to_par = hole_service.map_hole_ids_to_par(hole_ids=hole_ids)
+            # TODO: this is a hack, num_holes should be a property of golf_round
+            if len(golf_round.stats) == 18:
+                stats_summary = self.calculate_stats_summary(
+                    stats=golf_round.stats,
+                    hole_ids_to_par=hole_ids_to_par,
+                )
+                data[golf_round.id] = stats_summary
+
+        response_body = {
+            'status': 'success',
+            'uri': f'/golf-round-stats/summary',
+            'result': data,
         }
         return make_response(jsonify(response_body), HTTPStatus.OK.value)
