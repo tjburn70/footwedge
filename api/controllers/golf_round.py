4ed1ba77384b218ee362ab937c7b938029575122
Tristan Burnham <tjburn70@gmail.com>
2020-09-29 12:40:26 -0500

    Add new /summary endpoint

--- api/controllers/golf_round.py
+++ api/controllers/golf_round.py
@@ -10,12 +10,15 @@ from flask_jwt_extended import (
 from api.services import (
     golf_round_service,
     golf_round_stats_service,
+    hole_service,
 )
 from api.repositories.golf_round_repository import golf_round_repo
 from api.repositories.golf_round_stats_repository import golf_round_stats_repo
+from api.repositories.hole_repository import hole_repo
 from api.schemas import (
     GolfRoundSchema,
-    GolfRoundStatsSchema
+    GolfRoundStatsSchema,
+    HoleSchema
 )
 from api.helpers import requires_json_content
 
@@ -23,6 +26,7 @@ from api.helpers import requires_json_content
 blueprint = Blueprint('golf-rounds', __name__)
 golf_round_schema = GolfRoundSchema()
 golf_round_stats_schema = GolfRoundStatsSchema()
+hole_schema = HoleSchema()
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
@@ -54,3 +58,20 @@ def golf_round_stats(golf_round_id):
 
     payload = request.get_json()
     return service.add(golf_round_id=golf_round_id, payload=payload)
+
+
+@blueprint.route('/golf-round-stats/summary', methods=['GET'])
+@requires_json_content
+@jwt_required
+def stats_summary():
+    user_id = get_jwt_identity()
+    stats_service = golf_round_stats_service.GolfRoundStatsService(
+        repo=golf_round_stats_repo,
+        schema=golf_round_stats_schema,
+    )
+    rounds = golf_round_repo.get_by_user_id(user_id=user_id)
+    hole_svc = hole_service.HoleService(
+        repo=hole_repo,
+        schema=hole_schema,
+    )
+    return stats_service.stats_summary_by_round_id(golf_rounds=rounds, hole_service=hole_svc)
