7685075ca4d822e8931b47dd62a4785b7294a11f
Tristan Burnham <tjburn70@gmail.com>
2020-09-28 20:54:00 -0500

    Utilize services in golf_course controller

--- api/controllers/golf_course.py
+++ api/controllers/golf_course.py
@@ -1,13 +1,13 @@
-from http import HTTPStatus
-
 from flask import (
     Blueprint,
-    jsonify,
-    make_response,
     request
 )
-from marshmallow import ValidationError
 
+from api.services import (
+    golf_course_service,
+    tee_box_service,
+    hole_service,
+)
 from api.repositories.golf_course_repository import golf_course_repo
 from api.repositories.tee_box_repository import tee_box_repo
 from api.repositories.hole_repository import hole_repo
@@ -32,154 +32,66 @@ hole_schema = HoleSchema()
 @throws_500_on_exception
 def golf_courses():
     ids = request.args.getlist('id')
+    service = golf_course_service.GolfCourseService(
+        repo=golf_course_repo,
+        schema=golf_course_schema,
+    )
     if ids:
-        courses = golf_course_repo.get_by_ids(ids=ids)
-        results = golf_course_schema.dump(courses, many=True)
-        response_body = {
-            'status': 'success',
-            'result': results.data
-        }
-        return make_response(jsonify(response_body), HTTPStatus.OK.value)
+        return service.get_by_ids(ids=ids)
 
-    courses = golf_course_repo.get_all()
-    results = golf_course_schema.dump(courses, many=True)
-    response_body = {
-        'status': 'success',
-        'result': results.data
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
+    return service.get_all()
 
 
 @blueprint.route('/<int:golf_course_id>', methods=['GET'])
 @throws_500_on_exception
 def golf_courses_by_id(golf_course_id):
-    golf_course = golf_course_repo.get(golf_course_id)
-    if not golf_course:
-        response_body = {
-            'status': 'fail',
-            'message': f"No GolfCourse exists with id: '{golf_course_id}'"
-        }
-        return make_response(jsonify(response_body), HTTPStatus.BAD_REQUEST.value)
-
-    result = golf_course_schema.dump(golf_course)
-    response_body = {
-        'status': 'success',
-        'result': result
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
+    service = golf_course_service.GolfCourseService(
+        repo=golf_course_repo,
+        schema=golf_course_schema,
+    )
+    return service.get(_id=golf_course_id)
 
 
 @blueprint.route('/<int:golf_course_id>/tee-boxes', methods=['GET', 'POST'])
 @requires_json_content
 @throws_500_on_exception
 def tee_boxes(golf_course_id):
+    service = tee_box_service.TeeBoxService(
+        repo=tee_box_repo,
+        schema=tee_box_schema,
+    )
     if request.method == 'GET':
-        boxes = tee_box_repo.get_by_golf_course_id(golf_course_id=golf_course_id)
-        if not boxes:
-            response_body = {
-                'status': 'fail',
-                'message': f"No TeeBoxes exists with GolfCourse id: '{golf_course_id}'"
-            }
-            return make_response(jsonify(response_body), HTTPStatus.BAD_REQUEST.value)
-        else:
-            results = tee_box_schema.dump(boxes, many=True)
-            response_body = {
-                'status': 'success',
-                'result': results.data
-            }
-            return make_response(jsonify(response_body), HTTPStatus.OK.value)
+        return service.get_by_golf_course_id(golf_course_id=golf_course_id)
 
-    request_body = request.get_json()
-    request_body['golf_course_id'] = golf_course_id
-    try:
-        tee_box_data = tee_box_schema.load(request_body).data
-    except ValidationError as e:
-        response_body = {
-            'status': 'fail',
-            'message': e.messages
-        }
-        return make_response(jsonify(response_body), HTTPStatus.UNPROCESSABLE_ENTITY.value)
-
-    tee_box = tee_box_repo.create(data=tee_box_data)
-    tee_box_id = tee_box.id
-    response_body = {
-        'status': 'success',
-        'message': f"Tee Box: '{tee_box.tee_color}' was successfully added",
-        'uri': f'/golf-courses/{golf_course_id}/tee-boxes/{tee_box_id}',
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
+    payload = request.get_json()
+    return service.add(golf_course_id=golf_course_id, payload=payload)
 
 
 # TODO: Consider diff controller?
 @blueprint.route('/tee-boxes/<int:tee_box_id>', methods=['GET'])
 @throws_500_on_exception
 def tee_box_by_id(tee_box_id):
-    box = tee_box_repo.get(id=tee_box_id)
-    if not box:
-        response_body = {
-            'status': 'fail',
-            'message': f"No TeeBox exists with id: '{tee_box_id}'"
-        }
-        return make_response(jsonify(response_body), HTTPStatus.BAD_REQUEST.value)
-
-    data = tee_box_schema.dump(box).data
-    response_body = {
-        'status': 'success',
-        'result': data
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
+    service = tee_box_service.TeeBoxService(
+        repo=tee_box_repo,
+        schema=tee_box_schema,
+    )
+    return service.get(_id=tee_box_id)
 
 
 @blueprint.route('/<int:golf_course_id>/tee-boxes/<int:tee_box_id>/holes', methods=['GET', 'POST'])
 @requires_json_content
 @throws_500_on_exception
 def holes(golf_course_id, tee_box_id):
+    service = hole_service.HoleService(
+        repo=hole_repo,
+        schema=hole_schema,
+    )
     if request.method == 'GET':
-        hole_records = hole_repo.get_by_tee_box_id(tee_box_id=tee_box_id)
-        if not hole_records:
-            response_body = {
-                'status': 'fail',
-                'message': f"No Holes exist for TeeBox id: '{tee_box_id}'"
-            }
-            return make_response(jsonify(response_body), HTTPStatus.BAD_REQUEST.value)
-        else:
-            results = hole_schema.dump(hole_records, many=True)
-            response_body = {
-                'status': 'success',
-                'result': results.data
-            }
-            return make_response(jsonify(response_body), HTTPStatus.OK.value)
+        return service.get_by_tee_box_id(tee_box_id=tee_box_id)
 
-    request_body = request.get_json()
-    if not request_body.get('holes'):
-        response_body = {
-            'status': 'fail',
-            'message': "Request is missing required parameter: 'holes'"
-        }
-        return make_response(jsonify(response_body), HTTPStatus.BAD_REQUEST.value)
-
-    holes_data = []
-    for hole in request_body.get('holes'):
-        hole['golf_course_id'] = golf_course_id
-        hole['tee_box_id'] = tee_box_id
-        try:
-            hole_data = hole_schema.load(hole).data
-        except ValidationError as e:
-            response_body = {
-                'status': 'fail',
-                'message': e.messages
-            }
-            return make_response(jsonify(response_body), HTTPStatus.UNPROCESSABLE_ENTITY.value)
-
-        holes_data.append(hole_data)
-
-    hole_repo.bulk_create(records=holes_data)
-
-    message = "Hole records were successfully added for" \
-              f" GolfCourse id: {golf_course_id} and TeeBox id: {tee_box_id}"
-    response_body = {
-        'status': 'success',
-        'message': message,
-        'uri': f'/golf-courses/{golf_course_id}/tee-boxes/{tee_box_id}/holes/',
-    }
-    return make_response(jsonify(response_body), HTTPStatus.OK.value)
+    payload = request.get_json()
+    return service.add(
+        golf_course_id=golf_course_id,
+        tee_box_id=tee_box_id,
+        payload=payload
+    )
