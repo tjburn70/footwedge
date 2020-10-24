fb59f8e7abf44684414cfcae3110da3445d6b0d6
Tristan Burnham <tjburn70@gmail.com>
2020-10-23 19:09:58 -0500

    Add FOOTWEDGE_DATABASE_URI and REDIS_URI to settings

--- api/settings.py
+++ api/settings.py
@@ -1,9 +1,11 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyHttpUrl
 
 
 class Settings(BaseSettings):
-    HANDICAP_QUEUE_URL: str
-    SEARCH_SERVICE_API_BASE_URL: str
+    HANDICAP_QUEUE_URL: AnyHttpUrl
+    SEARCH_SERVICE_API_BASE_URL: AnyHttpUrl
+    FOOTWEDGE_DATABASE_URI: str
+    REDIS_URI: str
 
 
 settings = Settings(_env_file='./api/.env', _env_file_encoding='utf-8')
