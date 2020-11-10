d2daf95d56a8936272c14ce0631f3b124a4a56b1
Tristan Burnham <tjburn70@gmail.com>
2020-11-10 11:07:47 -0600

    Add a script run_integration_tests.sh to only run tests marked as integration

---
+++ lambdas/handicap-service/script/run_integration_tests.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# should be run from top-level directory in repo
+main() {
+  test_directory=$(pwd)/lambdas/handicap-service
+  PYTHONPATH="${test_directory}/src" python -m pytest -m "integration" "${test_directory}"
+}
+
+main
