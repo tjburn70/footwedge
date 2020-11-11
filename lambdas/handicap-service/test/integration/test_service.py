65c43a06050ce9f376670deefe3c35182fe8dbb7
Tristan Burnham <tjburn70@gmail.com>
2020-11-10 22:33:51 -0600

    Add a method to test add_handicap

--- lambdas/handicap-service/test/integration/test_service.py
+++ lambdas/handicap-service/test/integration/test_service.py
@@ -18,8 +18,7 @@ class TestHandicapService:
 
     @classmethod
     @pytest.fixture(autouse=True)
-    def setup_class(cls, golf_club, golf_course_factory, tee_box_factory,
-                    user, golf_round_factory, footwedge_api_client):
+    def setup_class(cls, golf_club, golf_course_factory, tee_box_factory, user, footwedge_api_client):
         cls.golf_club_id = golf_club.get('id')
 
         cls.golf_course_data = golf_course_factory(golf_club_id=cls.golf_club_id)
@@ -40,17 +39,6 @@ class TestHandicapService:
         cls.access_token = user.get('access_token')
         cls.refresh_token = user.get('refresh_token')
 
-        cls.scores = [74, 75, 76, 77, 78]
-        cls.golf_rounds = []
-        for score in cls.scores:
-            golf_round_data = golf_round_factory(
-                access_token=cls.access_token,
-                golf_course_id=cls.golf_course_id,
-                tee_box_id=cls.tee_box_id,
-                gross_score=score,
-            )
-            cls.golf_rounds.append(GolfRound(**golf_round_data))
-
         cls.footwedge_api_client = footwedge_api_client
         cls.handicap_service = HandicapService(
             footwedge_api_client=footwedge_api_client,
@@ -65,10 +53,6 @@ class TestHandicapService:
         )
         with pytest.raises(HandicapServiceFailure):
             handicap_svc._get_golf_rounds()
-
-    def test_get_golf_rounds(self):
-        results = self.handicap_service._get_golf_rounds()
-        assert results == self.golf_rounds, f"Expected results to = {self.golf_rounds}"
 
     def test_get_tee_box_invalid_id(self):
         invalid_tee_box_id = IMPOSSIBLY_LARGE_INT
@@ -104,3 +88,38 @@ class TestHandicapService:
         handicap_svc.add_handicap()
 
         assert not mock_post_handicap.called
+
+    def test_add_handicap(self, golf_round_factory):
+        scores = [74, 75, 76, 77, 78]
+        for score in scores:
+            golf_round_factory(
+                access_token=self.access_token,
+                golf_course_id=self.golf_course_id,
+                tee_box_id=self.tee_box_id,
+                gross_score=score,
+            )
+
+        self.handicap_service.add_handicap()
+
+        method = "get"
+        path = f"/handicaps"
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        resp = self.footwedge_api_client.call(
+            method=method,
+            path=path,
+            headers=headers,
+        )
+        resp.raise_for_status()
+        result = resp.json()['result']
+
+        handicap_id = result.get('id')
+        index = result.get('index')
+        lowest_score = min(scores)
+        differential = HandicapService.calculate_differential(
+            gross_score=lowest_score,
+            slope=self.slope,
+            course_rating=self.course_rating,
+        )
+        expected_index = round(differential * Decimal('0.96'), 1)
+
+        assert Decimal(index) == expected_index, f"Expecting an index of: {expected_index}"
