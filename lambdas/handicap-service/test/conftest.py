9b0c2f1d66c4f80f776b1435005e588f3e64fcf7
Tristan Burnham <tjburn70@gmail.com>
2020-11-12 23:22:37 -0600

    try an overhaul on the TestHandicapService

--- lambdas/handicap-service/test/conftest.py
+++ lambdas/handicap-service/test/conftest.py
@@ -25,12 +25,12 @@ def pytest_configure(config):
     )
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="session")
 def footwedge_api_client():
     return FootwedgeApi()
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="session")
 def golf_club(footwedge_api_client: FootwedgeApi):
     request_body = {
         "name": "Test Golf Club",
@@ -69,84 +69,156 @@ def golf_club(footwedge_api_client: FootwedgeApi):
         print(f"Unable to delete golf_club with id: {golf_club_id}")
 
 
-@pytest.fixture(scope="class")
-def golf_course_factory(footwedge_api_client: FootwedgeApi):
-    golf_course_ids = []
+@pytest.fixture(scope="session")
+def golf_course(footwedge_api_client: FootwedgeApi, golf_club):
+    golf_club_id = golf_club.get('id')
+    request_body = {
+        "name": "Test Golf Course",
+        "num_holes": 18,
+    }
+    method = "post"
+    path = f"/golf-clubs/{golf_club_id}/golf-courses"
+    headers = {"Content-Type": "application/json"}
+    resp = footwedge_api_client.call(
+        method=method,
+        path=path,
+        headers=headers,
+        data=json.dumps(request_body, default=str)
+    )
+    resp.raise_for_status()
+    result = resp.json()['result']
+    golf_course_id = result.get('id')
+    yield result
 
-    def _golf_course_factory(golf_club_id: int):
-        request_body = {
-            "name": "Test Golf Course",
-            "num_holes": 18,
-        }
-        method = "post"
-        path = f"/golf-clubs/{golf_club_id}/golf-courses"
-        headers = {"Content-Type": "application/json"}
-        resp = footwedge_api_client.call(
-            method=method,
-            path=path,
-            headers=headers,
-            data=json.dumps(request_body, default=str)
-        )
-        resp.raise_for_status()
-        result = resp.json()['result']
-        golf_course_ids.append(result.get('id'))
-        return result
-
-    yield _golf_course_factory
-
-    print("Cleaning up golf_course_factory...")
-    for golf_course_id in golf_course_ids:
-        delete_path = f"/golf-courses/{golf_course_id}"
-        del_resp = footwedge_api_client.call(
-            method="delete",
-            path=delete_path,
-        )
-        if del_resp.status_code == 204:
-            print(f"Successfully deleted golf_course with id: {golf_course_id}")
-        else:
-            print(f"Unable to delete golf_course with id: {golf_course_id}")
+    delete_path = f"/golf-courses/{golf_course_id}"
+    del_resp = footwedge_api_client.call(
+        method="delete",
+        path=delete_path,
+    )
+    if del_resp.status_code == 204:
+        print(f"Successfully deleted golf_course with id: {golf_course_id}")
+    else:
+        print(f"Unable to delete golf_course with id: {golf_course_id}")
 
 
-@pytest.fixture(scope="class")
-def tee_box_factory(footwedge_api_client: FootwedgeApi):
-    tee_box_ids = []
+# @pytest.fixture(scope="class")
+# def golf_course_factory(footwedge_api_client: FootwedgeApi):
+#     golf_course_ids = []
+#
+#     def _golf_course_factory(golf_club_id: int):
+#         request_body = {
+#             "name": "Test Golf Course",
+#             "num_holes": 18,
+#         }
+#         method = "post"
+#         path = f"/golf-clubs/{golf_club_id}/golf-courses"
+#         headers = {"Content-Type": "application/json"}
+#         resp = footwedge_api_client.call(
+#             method=method,
+#             path=path,
+#             headers=headers,
+#             data=json.dumps(request_body, default=str)
+#         )
+#         resp.raise_for_status()
+#         result = resp.json()['result']
+#         golf_course_ids.append(result.get('id'))
+#         return result
+#
+#     yield _golf_course_factory
+#
+#     print("Cleaning up golf_course_factory...")
+#     for golf_course_id in golf_course_ids:
+#         delete_path = f"/golf-courses/{golf_course_id}"
+#         del_resp = footwedge_api_client.call(
+#             method="delete",
+#             path=delete_path,
+#         )
+#         if del_resp.status_code == 204:
+#             print(f"Successfully deleted golf_course with id: {golf_course_id}")
+#         else:
+#             print(f"Unable to delete golf_course with id: {golf_course_id}")
 
-    def _tee_box_factory(golf_course_id: int, par: int, course_rating: Decimal, slope: Decimal):
-        request_body = {
-            "tee_color": "Blue",
-            "par": par,
-            "distance": "6305",
-            "unit": "yards",
-            "course_rating": course_rating,
-            "slope": slope,
-        }
-        method = "post"
-        path = f"/golf-courses/{golf_course_id}/tee-boxes"
-        headers = {"Content-Type": "application/json"}
-        resp = footwedge_api_client.call(
-            method=method,
-            path=path,
-            headers=headers,
-            data=json.dumps(request_body, default=str)
-        )
-        resp.raise_for_status()
-        result = resp.json()['result']
-        tee_box_ids.append(result.get('id'))
-        return result
 
-    yield _tee_box_factory
+@pytest.fixture(scope="session")
+def tee_box(footwedge_api_client: FootwedgeApi, golf_course):
+    par = 72
+    course_rating = Decimal(73.5)
+    slope = Decimal(134.0)
+    golf_course_id = golf_course.get('id')
+    request_body = {
+        "tee_color": "Blue",
+        "par": par,
+        "distance": "6305",
+        "unit": "yards",
+        "course_rating": course_rating,
+        "slope": slope,
+    }
+    method = "post"
+    path = f"/golf-courses/{golf_course_id}/tee-boxes"
+    headers = {"Content-Type": "application/json"}
+    resp = footwedge_api_client.call(
+        method=method,
+        path=path,
+        headers=headers,
+        data=json.dumps(request_body, default=str)
+    )
+    resp.raise_for_status()
+    result = resp.json()['result']
+    tee_box_id = result.get('id')
+    yield result
 
     print("Cleaning up tee_box_factory...")
-    for tee_box_id in tee_box_ids:
-        delete_path = f"/golf-courses/tee-boxes/{tee_box_id}"
-        del_resp = footwedge_api_client.call(
-            method="delete",
-            path=delete_path,
-        )
-        if del_resp.status_code == 204:
-            print(f"Successfully deleted tee_box with id: {tee_box_id}")
-        else:
-            print(f"Unable to delete tee_box with id: {tee_box_id}")
+    delete_path = f"/golf-courses/tee-boxes/{tee_box_id}"
+    del_resp = footwedge_api_client.call(
+        method="delete",
+        path=delete_path,
+    )
+    if del_resp.status_code == 204:
+        print(f"Successfully deleted tee_box with id: {tee_box_id}")
+    else:
+        print(f"Unable to delete tee_box with id: {tee_box_id}")
+
+
+# @pytest.fixture(scope="class")
+# def tee_box_factory(footwedge_api_client: FootwedgeApi):
+#     tee_box_ids = []
+#
+#     def _tee_box_factory(golf_course_id: int, par: int, course_rating: Decimal, slope: Decimal):
+#         request_body = {
+#             "tee_color": "Blue",
+#             "par": par,
+#             "distance": "6305",
+#             "unit": "yards",
+#             "course_rating": course_rating,
+#             "slope": slope,
+#         }
+#         method = "post"
+#         path = f"/golf-courses/{golf_course_id}/tee-boxes"
+#         headers = {"Content-Type": "application/json"}
+#         resp = footwedge_api_client.call(
+#             method=method,
+#             path=path,
+#             headers=headers,
+#             data=json.dumps(request_body, default=str)
+#         )
+#         resp.raise_for_status()
+#         result = resp.json()['result']
+#         tee_box_ids.append(result.get('id'))
+#         return result
+#
+#     yield _tee_box_factory
+#
+#     print("Cleaning up tee_box_factory...")
+#     for tee_box_id in tee_box_ids:
+#         delete_path = f"/golf-courses/tee-boxes/{tee_box_id}"
+#         del_resp = footwedge_api_client.call(
+#             method="delete",
+#             path=delete_path,
+#         )
+#         if del_resp.status_code == 204:
+#             print(f"Successfully deleted tee_box with id: {tee_box_id}")
+#         else:
+#             print(f"Unable to delete tee_box with id: {tee_box_id}")
 
 
 @pytest.fixture(scope="class")
@@ -222,7 +294,7 @@ def golf_round_model_factory():
     return _golf_round_model_factory
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="session")
 def golf_round_factory(footwedge_api_client: FootwedgeApi):
     golf_round_ids = []
     auth_headers = {}
--- lambdas/handicap-service/test/integration/test_service.py
+++ lambdas/handicap-service/test/integration/test_service.py
@@ -16,56 +16,66 @@ IMPOSSIBLY_LARGE_INT = 10000000000000000000
 @pytest.mark.integration
 class TestHandicapService:
 
-    @classmethod
-    @pytest.fixture(autouse=True)
-    def setup_class(cls, golf_club, golf_course_factory, tee_box_factory, user, footwedge_api_client):
-        cls.golf_club_id = golf_club.get('id')
+    # @classmethod
+    # @pytest.fixture(autouse=True)
+    # def setup_class(cls, golf_club, golf_course_factory, tee_box_factory, user, footwedge_api_client):
+    #     cls.golf_club_id = golf_club.get('id')
+    #
+    #     cls.golf_course_data = golf_course_factory(golf_club_id=cls.golf_club_id)
+    #     cls.golf_course_id = cls.golf_course_data.get('id')
+    #
+    #     cls.par = 72
+    #     cls.course_rating = Decimal(73.5)
+    #     cls.slope = Decimal(134.0)
+    #     cls.tee_box_data = tee_box_factory(
+    #         golf_course_id=cls.golf_course_id,
+    #         par=cls.par,
+    #         course_rating=cls.course_rating,
+    #         slope=cls.slope,
+    #     )
+    #     cls.tee_box_id = cls.tee_box_data.get('id')
+    #
+    #     cls.user_id = user.get('user_id')
+    #     cls.access_token = user.get('access_token')
+    #     cls.refresh_token = user.get('refresh_token')
+    #
+    #     cls.footwedge_api_client = footwedge_api_client
+    #     cls.handicap_service = HandicapService(
+    #         footwedge_api_client=footwedge_api_client,
+    #         user_id=cls.user_id,
+    #     )
 
-        cls.golf_course_data = golf_course_factory(golf_club_id=cls.golf_club_id)
-        cls.golf_course_id = cls.golf_course_data.get('id')
-
-        cls.par = 72
-        cls.course_rating = Decimal(73.5)
-        cls.slope = Decimal(134.0)
-        cls.tee_box_data = tee_box_factory(
-            golf_course_id=cls.golf_course_id,
-            par=cls.par,
-            course_rating=cls.course_rating,
-            slope=cls.slope,
-        )
-        cls.tee_box_id = cls.tee_box_data.get('id')
-
-        cls.user_id = user.get('user_id')
-        cls.access_token = user.get('access_token')
-        cls.refresh_token = user.get('refresh_token')
-
-        cls.footwedge_api_client = footwedge_api_client
-        cls.handicap_service = HandicapService(
-            footwedge_api_client=footwedge_api_client,
-            user_id=cls.user_id,
-        )
-
-    def test_get_golf_rounds_user_no_golf_rounds(self):
+    def test_get_golf_rounds_user_no_golf_rounds(self, footwedge_api_client):
         invalid_user_id = IMPOSSIBLY_LARGE_INT
         handicap_svc = HandicapService(
-            footwedge_api_client=self.footwedge_api_client,
+            footwedge_api_client=footwedge_api_client,
             user_id=invalid_user_id
         )
         with pytest.raises(HandicapServiceFailure):
             handicap_svc._get_golf_rounds()
 
-    def test_get_tee_box_invalid_id(self):
+    def test_get_tee_box_invalid_id(self, footwedge_api_client):
         invalid_tee_box_id = IMPOSSIBLY_LARGE_INT
+        handicap_service = HandicapService(
+            footwedge_api_client=footwedge_api_client,
+            user_id=1,
+        )
         with pytest.raises(HandicapServiceFailure):
-            self.handicap_service._get_tee_box(
+            handicap_service._get_tee_box(
                 tee_box_id=invalid_tee_box_id,
             )
 
-    def test_get_tee_box(self):
-        actual_tee_box = self.handicap_service._get_tee_box(
-            tee_box_id=self.tee_box_id,
+    def test_get_tee_box(self, footwedge_api_client, tee_box):
+        tee_box_id = tee_box.get('id')
+
+        handicap_service = HandicapService(
+            footwedge_api_client=footwedge_api_client,
+            user_id=1,
         )
-        expected_tee_box = TeeBox(**self.tee_box_data)
+        actual_tee_box = handicap_service._get_tee_box(
+            tee_box_id=tee_box_id,
+        )
+        expected_tee_box = TeeBox(**tee_box)
         assert actual_tee_box == expected_tee_box, f"Expected a result = {expected_tee_box.json()}"
 
     @patch(f'{HANDICAP_SERVICE_IMPORT_PATH}.post_handicap')
@@ -73,15 +83,18 @@ class TestHandicapService:
     def test_post_handicap_not_enough_golf_rounds(self,
                                                   mock_get_golf_rounds,
                                                   mock_post_handicap,
+                                                  footwedge_api_client,
+                                                  tee_box,
                                                   golf_round_model_factory):
         user_id_not_enough_rounds = -1
+        tee_box_id = tee_box.get('id')
         golf_round = golf_round_model_factory(
             user_id=user_id_not_enough_rounds,
-            tee_box_id=self.tee_box_id,
+            tee_box_id=tee_box_id,
         )
         mock_get_golf_rounds.return_value = [golf_round]
         handicap_svc = HandicapService(
-            footwedge_api_client=self.footwedge_api_client,
+            footwedge_api_client=footwedge_api_client,
             user_id=user_id_not_enough_rounds
         )
 
@@ -89,22 +102,28 @@ class TestHandicapService:
 
         assert not mock_post_handicap.called
 
-    def test_add_handicap(self, golf_round_factory):
+    def test_add_handicap(self, footwedge_api_client, user, golf_course, tee_box, golf_round_factory):
+        access_token = user.get('access_token')
+        golf_course_id = golf_course.get('id')
+        tee_box_id = tee_box.get('id')
         scores = [74, 75, 76, 77, 78]
         for score in scores:
             golf_round_factory(
-                access_token=self.access_token,
-                golf_course_id=self.golf_course_id,
-                tee_box_id=self.tee_box_id,
+                access_token=access_token,
+                golf_course_id=golf_course_id,
+                tee_box_id=tee_box_id,
                 gross_score=score,
             )
 
-        self.handicap_service.add_handicap()
+        HandicapService(
+            footwedge_api_client=footwedge_api_client,
+            user_id=user.get('user_id')
+        ).add_handicap()
 
         method = "get"
         path = f"/handicaps"
-        headers = {"Authorization": f"Bearer {self.access_token}"}
-        resp = self.footwedge_api_client.call(
+        headers = {"Authorization": f"Bearer {access_token}"}
+        resp = footwedge_api_client.call(
             method=method,
             path=path,
             headers=headers,
@@ -115,10 +134,12 @@ class TestHandicapService:
         handicap_id = result.get('id')
         index = result.get('index')
         lowest_score = min(scores)
+        slope = Decimal(tee_box.get('slope'))
+        course_rating = Decimal(tee_box.get('course_rating'))
         differential = HandicapService.calculate_differential(
             gross_score=lowest_score,
-            slope=self.slope,
-            course_rating=self.course_rating,
+            slope=slope,
+            course_rating=course_rating,
         )
         expected_index = round(differential * Decimal('0.96'), 1)
 
