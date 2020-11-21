3d16f323bfd0409ef2b11278aef1a0c372bdb97c
Tristan Burnham <tjburn70@gmail.com>
2020-11-21 12:53:25 -0600

    Utilize style-loader and css-loader to handle .css file

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -23,6 +23,10 @@ const config = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
